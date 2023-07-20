db52d3e194ba318fac2c86e33e9a7a35a8d6b2b5
Linh Ngo <lngo@clemson.edu>
2023-07-20 03:54:40 -0400

    two apps: jupyter and rstudio

--- template/after.sh
+++ template/after.sh
@@ -1,11 +1,11 @@
-# Wait for the Jupyter Notebook server to start
-echo "Waiting for Jupyter Notebook server to open port ${port}..."
+# Wait for the application server to start
+echo "Waiting for the application server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "${host}:${port}" 300; then
-  echo "Discovered Jupyter Notebook server listening on port ${port}!"
+  echo "Discovered application server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
-  echo "Timed out waiting for Jupyter Notebook server to open port ${port}!"
+  echo "Timed out waiting for application server to open port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
   pkill -P ${SCRIPT_PID}
   clean_up 1
